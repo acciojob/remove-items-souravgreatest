064c233bd0fff9b8f72453e96f0876194aae9e4d
sourav jain <95373772+souravgreatest@users.noreply.github.com>
2025-03-07 22:58:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+let arr=['Red','Green','White','Black']
+function solve()
+{
+	let val=document.getElementById('colorSelect').value;
+	let form=document.querySelector('#form');
+	let sela=document.getElementById('colorSelect');
+	sela.innerHTML="";
+	arr = arr.filter(color => color !== val);
+	arr.forEach((item)=>{
+		// if(item!==val)
+		// {
+			sela.innerHTML+=`
+				<option>${item}</option> 
+			`
+		// }
+	})
+}
